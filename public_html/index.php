be9bc48e2c53dc495d40afa0a33e12dee46a002c
Maria Rosado <mrosado2@cnm.edu>
2016-11-26 15:24:07 -0700

    test

---
+++ public_html/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lsng="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<!-- meta viewport tag -->
+		<meta name="viewport" content="width=device=width, initial-scale=1"/>
+		<link href
+		<!-- add CSS file -->
+		<link href="css/stylesheet.css" rel="stylesheet" type="text/css" />
+		<title>HTML Sandbox</title>
+	</head>
+	<body>
+		<!-- this is the header -->
+		<header class="bg-gray">
+			<h1 id="page-title">Hi! This is my page.</h1>
+		</header>
+		<!-- this is the main page content -->
+		<main>
+			<div>This is not in a section tag.</div>
+			<!-- begin section 1 -->
+			<section class="text-red">
+				<div>This is in a section tag.</div>
+				<p>Then maybe you should consider this: if anything happens to them, Starfleet is going to want a full investigation. I'll alert the crew. This should be interesting. Ensign Babyface! I can't. As much as I care about you, my first duty is to the ship. Could someone survive inside a transporter buffer for 75 years? The Federation's gone; the Borg is everywhere! When has justice ever been as simple as a rule book? Fear is the true enemy, the only enemy. Talk about going nowhere fast. The Enterprise computer system is controlled by three primary main processor cores, cross-linked with a redundant melacortz ramistat, fourteen kiloquad interface modules. Did you come here for something in particular or just general Riker-bashing? Besides, you look good in a dress. We know you're dealing in stolen ore. But I wanna talk about the assassination attempt on Lieutenant Worf. Yes, absolutely, I do indeed concur, wholeheartedly! In all trust, there is the possibility for betrayal.</p>
+				<img src="img/pikachu.png" alt="pikachu">
+				<p>Then maybe you should consider this: if anything happens to them, Starfleet is going to want a full investigation. I'll alert the crew. This should be interesting. Ensign Babyface! I can't. As much as I care about you, my first duty is to the ship. Could someone survive inside a transporter buffer for 75 years?</p>
+				<p>The Federation's gone; the Borg is everywhere! When has justice ever been as simple as a rule book? Fear is the true enemy, the only enemy. Talk about going nowhere fast. The Enterprise computer system is controlled by three primary main processor cores, cross-linked with a redundant melacortz ramistat, fourteen kiloquad interface modules. Did you come here for something in particular or just general Riker-bashing? Besides, you look good in a dress. We know you're dealing in stolen ore. But I wanna talk about the assassination attempt on Lieutenant Worf. Yes, absolutely, I do indeed concur, wholeheartedly! In all trust, there is the possibility for betrayal.</p>
+			</section>
+			<!-- begin section 2 -->
+			<section id="list-section" class="bg-gray">
+				<h2>My Favorite Things</h2>
+				<ul id="my-list">
+					<li class="foo">Senator Arlo</li>
+					<li class="foo">Star Wars</li>
+					<li class="foo">Beer</li>
+					<li class="foo">Donuts</li>
+				</ul>
+			</section>
+			<!-- begin section 3 -->
+			<section class="bg-gray">
+				<table>
+					<tr>
+						<th>Column One</th>
+						<th>Column Two</th>
+					</tr>
+					<tr>
+						<td>Cat</td>
+						<td>Dog</td>
+					</tr>
+					<tr>
+						<td>Car</td>
+						<td>Bike</td>
+					</tr>
+					<tr>
+						<td>Star Wars</td>
+						<td>Wine</td>
+					</tr>
+				</table>
+			</section>
+			<!-- start the box model -->
+			<div class="box-model">
+				<p>This is some text...</p>
+			</div>
+		</main>
+		</body>
+</html>
